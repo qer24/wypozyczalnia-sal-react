46eea24780d5affa1165efd2e4f83a0c481843bf
qer24 <qer24@go2.pl>
2025-01-31 00:22:07 +0100

    Theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import {createTheme, MantineProvider} from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import LoginButton from "./LoginButton.tsx";
 import {AuthProvider} from "./AuthContext.tsx";
+import {ThemeSwitchButton} from "./ThemeSwitchButton.tsx";
 
 const theme = createTheme({
     fontFamily: 'Open Sans, serif',
     primaryColor: 'violet',
-    primaryShade: 6,
+    primaryShade: { light: 4, dark: 7 },
     defaultRadius : 'lg',
-
 });
 
 export default function App() {
@@ -19,7 +19,10 @@ export default function App() {
     <AuthProvider>
         <MantineProvider defaultColorScheme="dark" theme={theme}>
             <ModalsProvider>
-                <LoginButton />
+                <div className="app">
+                    <ThemeSwitchButton />
+                    <LoginButton />
+                </div>
             </ModalsProvider>
         </MantineProvider>
     </AuthProvider>
---
+++ src/ThemeSwitchButton.tsx
@@ -0,0 +1,23 @@
+﻿import { useMantineColorScheme, ActionIcon } from '@mantine/core';
+import { IconBrightnessUp, IconMoon } from "@tabler/icons-react";
+
+export function ThemeSwitchButton() {
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+    return (
+        <ActionIcon
+            title="Toggle color scheme"
+            onClick={toggleColorScheme}
+            aria-label="Toggle color scheme"
+            variant="hover"
+            color="gray"
+            size="lg">
+
+            {colorScheme === 'dark' ? (
+                <IconMoon />
+            ) : (
+                <IconBrightnessUp />
+            )}
+        </ActionIcon>
+    );
+}
