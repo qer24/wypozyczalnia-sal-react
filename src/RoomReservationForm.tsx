2fffe108cecf3b34d3260e423c85a5fe342a1309
Kacper Przeździecki <86024@student.pb.edu.pl>
2025-02-02 09:41:03 +0100

    dodanie validacji dat rezerwacji

--- src/RoomReservationForm.tsx
+++ src/RoomReservationForm.tsx
@@ -1,6 +1,8 @@
-﻿import {Button, Group, Stack, TextInput} from "@mantine/core";
-import {useForm} from "@mantine/form";
-import {DateTimePicker} from "@mantine/dates";
+﻿import { Button, Group, Stack, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { DateTimePicker } from "@mantine/dates";
+import { useSelector } from "react-redux";
+import { RootState } from "./store"; // Popraw ścieżkę do RootState
 
 interface ReservationFormProps {
     onSubmit: (data: ReservationFormData) => void;
@@ -15,60 +17,104 @@ export interface ReservationFormData {
 }
 
 export function RoomReservationForm({ onSubmit }: ReservationFormProps) {
-    const form = useForm({
-        mode: 'uncontrolled',
-        initialValues: {
-            name: '',
-            lastName: '',
-            email: '',
-            start: null,
-            end: null,
-        },
-        validate: {
-            name: (value) => (value.length >= 2 ? null : 'Imię jest za krótkie'),
-            lastName: (value) => (value.length >= 2 ? null : 'Nazwisko jest za krótkie'),
-            email: (value) => (emailRegex.test(value) ? null : 'Nieprawidłowy email'),
-            start: (value) => (value ? null : 'Data początkowa jest wymagana'),
-            end: (value) => (value ? null : 'Data końcowa jest wymagana'),
-        },
-    })
+    // Pobieramy istniejące rezerwacje ze stanu Reduxa, aby sprawdzić nakładanie
+    const existingReservations = useSelector((state: RootState) => state.reservations);
 
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-    //const data = form.getValues();
+    const form = useForm({
+        mode: "uncontrolled",
+        initialValues: {
+            name: "",
+            lastName: "",
+            email: "",
+            start: null,
+            end: null,
+        },
+        validate: (values: ReservationFormData) => {
+            const errors: { [key: string]: string } = {};
+            const now = new Date();
+
+            // Walidacja imienia i nazwiska
+            if (!values.name || values.name.length < 2) {
+                errors.name = "Imię jest za krótkie";
+            }
+            if (!values.lastName || values.lastName.length < 2) {
+                errors.lastName = "Nazwisko jest za krótkie";
+            }
+
+            // Walidacja email
+            if (!values.email || !emailRegex.test(values.email)) {
+                errors.email = "Nieprawidłowy email";
+            }
+
+            // Walidacja daty początkowej
+            if (!values.start) {
+                errors.start = "Data początkowa jest wymagana";
+            } else if (values.start < now) {
+                errors.start = "Data początkowa nie może być z przeszłości";
+            }
+
+            // Walidacja daty końcowej
+            if (!values.end) {
+                errors.end = "Data końcowa jest wymagana";
+            } else if (values.end < now) {
+                errors.end = "Data końcowa nie może być z przeszłości";
+            } else if (values.start && values.end <= values.start) {
+                errors.end = "Data końcowa musi być późniejsza od daty początkowej";
+            }
+
+            // Sprawdzanie nakładania się rezerwacji
+            if (values.start && values.end && !errors.start && !errors.end) {
+                const overlappingReservation = existingReservations.find((reservation) => {
+                    const existingStart = new Date(reservation.startDateTime);
+                    const existingEnd = new Date(reservation.endDateTime);
+                    // Nowa rezerwacja nachodzi, jeśli:
+                    // nowa.dataPoczątkowa < istniejąca.dataKońcowa && nowa.dataKońcowa > istniejąca.dataPoczątkowa
+                    return values.start! < existingEnd && values.end! > existingStart;
+                });
+                if (overlappingReservation) {
+                    errors.start = "Rezerwacja nachodzi na już istniejącą rezerwację";
+                    errors.end = "Rezerwacja nachodzi na już istniejącą rezerwację";
+                }
+            }
+
+            return errors;
+        },
+    });
 
     return (
         <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
             <Stack>
                 <TextInput
                     placeholder="Imię"
-                    key={form.key('name')}
                     withAsterisk
-                    {...form.getInputProps('name')}/>
+                    {...form.getInputProps("name")}
+                />
 
                 <TextInput
                     placeholder="Nazwisko"
-                    key={form.key('lastName')}
                     withAsterisk
-                    {...form.getInputProps('lastName')}/>
+                    {...form.getInputProps("lastName")}
+                />
 
                 <TextInput
                     placeholder="Email"
-                    key={form.key('email')}
                     withAsterisk
-                    {...form.getInputProps('email')}/>
+                    {...form.getInputProps("email")}
+                />
 
                 <DateTimePicker
                     placeholder="Data początkowa"
-                    key={form.key('start')}
                     withAsterisk
-                    {...form.getInputProps('start')}/>
+                    {...form.getInputProps("start")}
+                />
 
                 <DateTimePicker
                     placeholder="Data końcowa"
-                    key={form.key('end')}
                     withAsterisk
-                    {...form.getInputProps('end')}/>
+                    {...form.getInputProps("end")}
+                />
 
                 <Group justify="center">
                     <Button type="submit">Dodaj</Button>
